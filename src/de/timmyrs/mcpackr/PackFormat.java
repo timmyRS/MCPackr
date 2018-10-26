4711b884abe222e54d7c9ef37832dfd6c9d45d46
Tim Speckhals <github@timmyrs.de>
2018-10-26 11:09:26 +0200

    Update pack format 4 mcversions to include 1.13.2

--- src/de/timmyrs/mcpackr/PackFormat.java
+++ src/de/timmyrs/mcpackr/PackFormat.java
@@ -5,7 +5,7 @@ public enum PackFormat
 	V1(1, "1.6.1 - 1.8.9"),
 	V2(2, "1.9 - 1.10.2"),
 	V3(3, "1.11 - 1.12.2"),
-	V4(4, "1.13 - 1.13.1");
+	V4(4, "1.13 - 1.13.2");
 
 	final public static PackFormat latest = PackFormat.V4;
 	final public int id;
